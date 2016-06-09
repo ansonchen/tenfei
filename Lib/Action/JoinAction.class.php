6c57975c7e0d195146400882a797bab6ae7bbb96
ansonchen <83655528@qq.com>
2016-06-09 18:54:08 +0800

    update

--- Lib/Action/JoinAction.class.php
+++ Lib/Action/JoinAction.class.php
@@ -18,12 +18,12 @@ class JoinAction extends CommonAction{
     }
     public function index(){
         
-    	
+    	import("ORG.Util.Page"); 
 	
 		$Article = D('Article');         
         $pid = 124;
 		//$pid = 123;
-        $this->assign( "ppid", $pid );
+        
         		
         
         if(!empty($_GET['type'])) {
@@ -36,24 +36,43 @@ class JoinAction extends CommonAction{
         $xpid = $Tlist['id'];
             
         }   
+		$where['bORn_article'] = 'n';
+        $where['menu_pid'] = $pid;
+        
+        $where['typeIds_article'] = $xpid;
+        // $pid = $_GET['pid'];
+        
+		$count = $Article->where($where)->count();//计算总数
+        
+        $p = new Page ( $count, 15 );
+		$Mlist=$Article->where($where)->limit($p->firstRow.','.$p->listRows)->order('id_article desc,writerId_article asc')->select();//findAll
+		$p->setConfig('header','记录');
 
-			
-        $Article	=	M("Article");
-            
-		$condition['typeIds_article']	=	$xpid;
-            
-		$vo = $Article->where($condition)->order('sort asc')->find(); // 查询数据   
-			if($vo) {
-				$this->assign('vo',$vo);
-				$this->display();
-			}else{
-				exit('页面不存在！');
-			}
-            
-              
+		$p->setConfig('prev',"&laquo; 上一页");
+		$p->setConfig('next','下一页 &raquo;');
+		$p->setConfig('first','&laquo; 第一页');
+		$p->setConfig('last','最后一页 &raquo;');
+		$page = $p->show ();
+		$this->assign( "page", $page );
+        $this->assign( "ppid", $pid );
+		$this->assign( "xpid", $xpid );	
+		//$Mlist = $Node->order('sort_node asc')->select(); 
+		
+		$this->assign('article',$Mlist);
+
+		
+		$this->display(); // 输出模板   
     }
 	
 	
+	//生成验证码
+	public function code() 
+    {
+		$type	 =	 isset($_GET['type'])?$_GET['type']:'gif';
+        import("ORG.Util.Image");
+		Image::UPCA('15819817119');
+       // Image::buildImageVerify(4,1,$type);
+    }
 
 }
 ?>
